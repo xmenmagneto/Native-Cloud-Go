46adcf51400f8b48345d9b446c9c1aa544dae128
Tingchang Deng <dengtingchang@gmail.com>
2018-03-26 08:58:56 -0700

    change statically defined port to port func

--- microcervice.go
+++ microcervice.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(port(), nil)
 }
 
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
